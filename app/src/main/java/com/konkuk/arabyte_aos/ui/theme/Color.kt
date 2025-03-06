4ec8dc5dfec10936ee14265586459ba4abfa6d48
t1nm1ksun <minsuk07672@naver.com>
2025-03-06 16:28:35 +0900

    init : Color 초기세팅

--- app/src/main/java/com/konkuk/arabyte_aos/ui/theme/Color.kt
+++ app/src/main/java/com/konkuk/arabyte_aos/ui/theme/Color.kt
@@ -1,11 +1,43 @@
 package com.konkuk.arabyte_aos.ui.theme
 
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+@Immutable
+data class ArabyteColors(
+    // Gray Scale
+    val black: Color,
+    val white: Color,
+    val gray08: Color,
+    val gray07: Color,
+    val gray06: Color,
+    val gray05: Color,
+    val gray04: Color,
+    val gray03: Color,
+    val gray02: Color,
+    val gray01: Color,
+    // Primary
+    val mainBlue: Color,
+    val subBlue: Color,
+    val lightBlue: Color,
+)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val arabyteColors =
+    ArabyteColors(
+        black = Color(0xFF000000),
+        gray08 = Color(0xFF1D1D1D),
+        gray07 = Color(0xFF373737),
+        gray06 = Color(0xFF585D62),
+        gray05 = Color(0xFF7B7C87),
+        gray04 = Color(0xFF999AAB),
+        gray03 = Color(0xFFD5D5DE),
+        gray02 = Color(0xFFE6E6EB),
+        gray01 = Color(0xFFF3F3F6),
+        white = Color(0xFFFFFFFF),
+        mainBlue = Color(0xFF5E76ED),
+        subBlue = Color(0xFF889CFC),
+        lightBlue = Color(0xFFE5E8FC),
+    )
+
+val LocalArabyteColors = staticCompositionLocalOf { arabyteColors }
