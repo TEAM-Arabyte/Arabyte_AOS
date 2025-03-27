0129f7c39494d6da5bdb0ed8892e59a1c2224dbd
t1nm1ksun <minsuk07672@naver.com>
2025-03-27 17:33:41 +0900

    chore#11: 토글버튼 분기처리 결과 align속성에 적용

--- app/src/main/java/com/konkuk/arabyte_aos/presentation/ui/component/button/ArabyteToggleButton.kt
+++ app/src/main/java/com/konkuk/arabyte_aos/presentation/ui/component/button/ArabyteToggleButton.kt
@@ -3,8 +3,6 @@ package com.konkuk.arabyte_aos.presentation.ui.component.button
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -14,6 +12,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,15 +27,27 @@ fun ArabyteToggleButton(
     modifier: Modifier = Modifier,
     buttonClicked: (Boolean) -> Unit = {},
 ) {
-    val backgroundColor = if (enabled) ArabyteTheme.colors.mainBlue else ArabyteTheme.colors.gray03
-    Box(modifier = modifier.roundedBackgroundWithPadding(cornerRadius = 20.dp, backgroundColor = backgroundColor).noRippleClickable { buttonClicked(!enabled) }) {
-        Row(modifier = Modifier.padding(2.5.dp)) {
-            if (!enabled) Spacer(modifier = Modifier.width(15.dp))
-            Spacer(modifier = Modifier.background(shape = CircleShape, color = ArabyteTheme.colors.white).size(15.dp))
-            if (enabled) Spacer(modifier = Modifier.width(15.dp))
-        }
+    val (backgroundColor, alignment) = when (enabled) {
+        true -> Pair(ArabyteTheme.colors.mainBlue, Alignment.CenterStart)
+        false -> Pair(ArabyteTheme.colors.gray03, Alignment.CenterEnd)
+    }
+
+    Box(
+        modifier = modifier
+            .width(35.dp)
+            .roundedBackgroundWithPadding(cornerRadius = 20.dp, backgroundColor = backgroundColor)
+            .noRippleClickable { buttonClicked(!enabled) },
+        contentAlignment = alignment
+    ) {
+        Box(
+            modifier = Modifier
+                .padding(2.5.dp)
+                .size(15.dp)
+                .background(color = ArabyteTheme.colors.white, shape = CircleShape)
+        )
     }
 }
+
 
 @Preview
 @Composable
