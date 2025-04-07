1aff5de263110dcb232aa9ce3357448a4b3d9cce
t1nm1ksun <minsuk07672@naver.com>
2025-04-07 18:04:15 +0900

    feat/#8: 안드로이드 기본 스플래시 삭제

--- app/src/main/java/com/konkuk/arabyte_aos/presentation/ui/splash/SplashScreen.kt
+++ app/src/main/java/com/konkuk/arabyte_aos/presentation/ui/splash/SplashScreen.kt
@@ -2,10 +2,8 @@ package com.konkuk.arabyte_aos.presentation.ui.splash
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -13,7 +11,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.konkuk.arabyte_aos.R
 import com.konkuk.arabyte_aos.ui.theme.ArabyteAOSTheme
 import com.konkuk.arabyte_aos.ui.theme.ArabyteTheme
@@ -21,19 +18,20 @@ import com.konkuk.arabyte_aos.ui.theme.ArabyteTheme
 @Composable
 fun SplashScreen(
     modifier: Modifier = Modifier,
-    innerPaddingValues: PaddingValues = PaddingValues(0.dp)
 ) {
     Column(
-        modifier = modifier
-            .fillMaxSize()
-            .background(color = ArabyteTheme.colors.mainBlue)
-            .padding(innerPaddingValues),
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier =
+            modifier
+                .fillMaxSize()
+                .background(color = ArabyteTheme.colors.mainBlue),
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         Spacer(modifier = Modifier.weight(207f))
         Text(
-            text = stringResource(R.string.splash_service_description), style = ArabyteTheme.typography.bodyMed15, textAlign = TextAlign.Center,
-            color = ArabyteTheme.colors.white
+            text = stringResource(R.string.splash_service_description),
+            style = ArabyteTheme.typography.bodyMed15,
+            textAlign = TextAlign.Center,
+            color = ArabyteTheme.colors.white,
         )
         Spacer(modifier = Modifier.weight(459f))
     }
