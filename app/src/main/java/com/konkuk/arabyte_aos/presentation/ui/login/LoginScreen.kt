162348e90d1286b12c3e3201b477bbfc8a9ac5e4
t1nm1ksun <minsuk07672@naver.com>
2025-04-07 18:04:40 +0900

    feat/#8: Splash 로직 구현, 상태 바 아이콘 색상 변경

--- app/src/main/java/com/konkuk/arabyte_aos/presentation/ui/login/LoginScreen.kt
+++ app/src/main/java/com/konkuk/arabyte_aos/presentation/ui/login/LoginScreen.kt
@@ -1,8 +1,13 @@
 package com.konkuk.arabyte_aos.presentation.ui.login
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun LoginScreen(modifier: Modifier = Modifier) {
+fun LoginScreen(
+    modifier: Modifier = Modifier,
+    innerPaddingValues: PaddingValues = PaddingValues(0.dp),
+) {
 }
