eca568ffd473cbee0d6d84c868ba513cef1587f5
t1nm1ksun <minsuk07672@naver.com>
2025-04-09 16:59:34 +0900

    feat#18: 닉네임 텍스트 필드 에러메세지 상수화

--- app/src/main/java/com/konkuk/arabyte_aos/presentation/util/Constraints.kt
+++ app/src/main/java/com/konkuk/arabyte_aos/presentation/util/Constraints.kt
@@ -11,4 +11,10 @@ object Token {
 object SignUp {
     const val MALE = "남성"
     const val FEMALE = "여성"
+
+    const val NICKNAME_IDLE_MESSAGE = ""
+    const val NICKNAME_VALID_MESSAGE = "닉네임은 공백 없이 한글로만 입력 가능합니다"
+    const val NICKNAME_INVALID_MESSAGE = "사용가능한 닉네임 입니다"
+
+    val errorMessageList = listOf(NICKNAME_IDLE_MESSAGE, NICKNAME_VALID_MESSAGE, NICKNAME_INVALID_MESSAGE)
 }
