7befcd8be1fcebb816c7274fd428fd4794e75daf
t1nm1ksun <minsuk07672@naver.com>
2025-04-13 21:47:29 +0900

    feat/#20: 온보딩 관련 enum 구현

--- app/src/main/java/com/konkuk/arabyte_aos/presentation/type/view/OnboardingType.kt
+++ app/src/main/java/com/konkuk/arabyte_aos/presentation/type/view/OnboardingType.kt
@@ -1,14 +1,12 @@
 package com.konkuk.arabyte_aos.presentation.type.view
 
-import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import com.konkuk.arabyte_aos.R
 
 enum class OnboardingType(
-    @DrawableRes val pageIconRes: Int,
     @StringRes val titleStringRes: Int,
     @StringRes val descriptionStringRes: Int,
 ) {
-    FIRST(pageIconRes = R.drawable.ic_all_auth_check_16, titleStringRes = R.string.sign_up_first_title, descriptionStringRes = R.string.sign_up_first_button),
-    SECOND(pageIconRes = R.drawable.ic_all_auth_check_16, titleStringRes = R.string.sign_up_second_title, descriptionStringRes = R.string.sign_up_second_button),
+    FIRST(titleStringRes = R.string.onboarding_first_title, descriptionStringRes = R.string.onboarding_first_description),
+    SECOND(titleStringRes = R.string.onboarding_second_title, descriptionStringRes = R.string.onboarding_second_description),
 }
