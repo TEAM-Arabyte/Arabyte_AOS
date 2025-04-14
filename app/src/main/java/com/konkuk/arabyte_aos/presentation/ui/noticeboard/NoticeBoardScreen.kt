652b89f52cd04c833805720d17d74aaaef83440c
hwidung <hwhwhw74@naver.com>
2025-04-15 00:12:08 +0900

    feat/#19: Bottom Navigation - NoticeBoard 네비게이션 구조

--- app/src/main/java/com/konkuk/arabyte_aos/presentation/ui/noticeboard/NoticeBoardScreen.kt
+++ app/src/main/java/com/konkuk/arabyte_aos/presentation/ui/noticeboard/NoticeBoardScreen.kt
@@ -1,8 +1,39 @@
 package com.konkuk.arabyte_aos.presentation.ui.noticeboard
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
-fun NoticeBoardScreen(modifier: Modifier = Modifier) {
+fun NoticeBoardRoute(
+    paddingValues: PaddingValues,
+    // navigateToMyInfoScreen : ()->Unit,
+    modifier: Modifier = Modifier,
+    viewModel: NoticeBoardViewModel = hiltViewModel(),
+) {
+    NoticeBoardScreen(
+        paddingValues = paddingValues,
+        modifier = modifier,
+        viewModel = viewModel,
+    )
+}
+
+@Composable
+fun NoticeBoardScreen(
+    paddingValues: PaddingValues,
+    // navigateToMyInfoScreen : ()->Unit,
+    modifier: Modifier = Modifier,
+    viewModel: NoticeBoardViewModel = hiltViewModel(),
+) {
+    Column(
+        modifier =
+            modifier
+                .fillMaxSize(),
+    ) {
+        Text(text = "NoticeBoard")
+    }
 }
---
+++ app/src/main/java/com/konkuk/arabyte_aos/presentation/ui/noticeboard/navigation/NoticeBoardNavigation.kt
@@ -0,0 +1,30 @@
+package com.konkuk.arabyte_aos.presentation.ui.noticeboard.navigation
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.konkuk.arabyte_aos.presentation.ui.noticeboard.NoticeBoardScreen
+
+fun NavController.navigationNoticeBoard() {
+    navigate(
+        route = NoticeBoardRoute.ROUTE,
+    ) {
+        popUpTo(graph.startDestinationId) { inclusive = true }
+        launchSingleTop = true
+    }
+}
+
+fun NavGraphBuilder.noticeboardNavGraph(
+    paddingValues: PaddingValues,
+) {
+    composable(route = NoticeBoardRoute.ROUTE) {
+        NoticeBoardScreen(
+            paddingValues = paddingValues,
+        )
+    }
+}
+
+object NoticeBoardRoute {
+    const val ROUTE = "NoticeBoard"
+}
