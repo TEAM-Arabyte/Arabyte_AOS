e760b447cfa0168ac987fd35daea4210b44a8d08
t1nm1ksun <minsuk07672@naver.com>
2025-04-14 15:14:20 +0900

    chore#10: 스플래시 뷰 로고 추가

--- app/src/main/java/com/konkuk/arabyte_aos/presentation/ui/splash/SplashScreen.kt
+++ app/src/main/java/com/konkuk/arabyte_aos/presentation/ui/splash/SplashScreen.kt
@@ -5,12 +5,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,14 +36,16 @@ fun SplashScreen(
                 .padding(innerPaddingValues),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Spacer(modifier = Modifier.weight(207f))
+        Spacer(modifier = Modifier.weight(225f))
+        Icon(imageVector = ImageVector.vectorResource(R.drawable.ic_all_logo), contentDescription = null, tint = Color.Unspecified)
+        Spacer(modifier = Modifier.height(10.dp))
         Text(
             text = stringResource(R.string.splash_service_description),
             style = ArabyteTheme.typography.bodyMed15,
             textAlign = TextAlign.Center,
             color = ArabyteTheme.colors.white,
         )
-        Spacer(modifier = Modifier.weight(459f))
+        Spacer(modifier = Modifier.weight(369f))
     }
 }
 
