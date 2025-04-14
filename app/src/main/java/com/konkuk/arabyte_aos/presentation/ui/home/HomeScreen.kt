71797e90a4b2a7da38f58f806bb289349c94767c
hwidung <hwhwhw74@naver.com>
2025-04-15 00:11:34 +0900

    feat/#19: Bottom Navigation - Home 네비게이션 구조

--- app/src/main/java/com/konkuk/arabyte_aos/presentation/ui/home/HomeScreen.kt
+++ app/src/main/java/com/konkuk/arabyte_aos/presentation/ui/home/HomeScreen.kt
@@ -1,8 +1,47 @@
 package com.konkuk.arabyte_aos.presentation.ui.home
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.konkuk.arabyte_aos.presentation.type.ArabyteCategoryType
 
 @Composable
-fun HomeScreen(modifier: Modifier = Modifier) {
+fun HomeRoute(
+    paddingValues: PaddingValues,
+    navigateToReview: (ArabyteCategoryType?) -> Unit,
+    navigateToNoticeBoard: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = hiltViewModel(),
+) {
+    HomeScreen(
+        paddingValues = paddingValues,
+        navigateToReview = navigateToReview,
+        navigateToNoticeBoard = navigateToNoticeBoard,
+        modifier = modifier,
+        viewModel = viewModel,
+    )
+}
+
+@Composable
+fun HomeScreen(
+    paddingValues: PaddingValues,
+    navigateToReview: (ArabyteCategoryType?) -> Unit,
+    navigateToNoticeBoard: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = hiltViewModel(),
+) {
+    Column(
+        modifier =
+            modifier
+                .fillMaxSize(),
+    ) {
+        Text(text = "Home")
+        Button(onClick = { navigateToReview(ArabyteCategoryType.MANAGEMENT) }) { Text("Review") }
+        Button(onClick = { navigateToNoticeBoard() }) { Text("NoticeBoard") }
+    }
 }
---
+++ app/src/main/java/com/konkuk/arabyte_aos/presentation/ui/home/navigation/HomeNavigation.kt
@@ -0,0 +1,36 @@
+package com.konkuk.arabyte_aos.presentation.ui.home.navigation
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.konkuk.arabyte_aos.presentation.type.ArabyteCategoryType
+import com.konkuk.arabyte_aos.presentation.ui.home.HomeRoute
+
+fun NavController.navigationHome() {
+    navigate(
+        route = HomeRoute.ROUTE,
+    ) {
+        popUpTo(graph.startDestinationId) { inclusive = true }
+        launchSingleTop = true
+    }
+}
+
+fun NavGraphBuilder.homeNavGraph(
+    paddingValues: PaddingValues,
+    navigateToReview: (ArabyteCategoryType?) -> Unit,
+    navigateToNoticeBoard: () -> Unit,
+    // MyProfileEditScreen, ReviewDetailScreen, BoardDetailScreen 등 추가
+) {
+    composable(route = HomeRoute.ROUTE) {
+        HomeRoute(
+            paddingValues = paddingValues,
+            navigateToReview = navigateToReview,
+            navigateToNoticeBoard = navigateToNoticeBoard,
+        )
+    }
+}
+
+object HomeRoute {
+    const val ROUTE = "Home"
+}
