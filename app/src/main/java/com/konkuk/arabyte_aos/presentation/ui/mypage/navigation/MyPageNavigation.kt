988126a6f19004f413b8bc1c619a8d3d5c012e44
hwidung <hwhwhw74@naver.com>
2025-04-15 00:12:17 +0900

    feat/#19: Bottom Navigation - MyPage 네비게이션 구조

---
+++ app/src/main/java/com/konkuk/arabyte_aos/presentation/ui/mypage/navigation/MyPageNavigation.kt
@@ -0,0 +1,30 @@
+package com.konkuk.arabyte_aos.presentation.ui.mypage.navigation
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.konkuk.arabyte_aos.presentation.ui.mypage.MyPageRoute
+
+fun NavController.navigationMyPage() {
+    navigate(
+        route = MyPageRoute.ROUTE,
+    ) {
+        popUpTo(graph.startDestinationId) { inclusive = true }
+        launchSingleTop = true
+    }
+}
+
+fun NavGraphBuilder.myPageNavGraph(
+    paddingValues: PaddingValues,
+) {
+    composable(route = MyPageRoute.ROUTE) {
+        MyPageRoute(
+            paddingValues = paddingValues,
+        )
+    }
+}
+
+object MyPageRoute {
+    const val ROUTE = "MyPage"
+}
