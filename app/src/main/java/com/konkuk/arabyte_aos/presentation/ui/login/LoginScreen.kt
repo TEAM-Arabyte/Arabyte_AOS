219d0b00965c7b62c9f8d37b4983d1d1f6f89458
t1nm1ksun <minsuk07672@naver.com>
2025-04-14 15:11:09 +0900

    chore#10: 로그인 뷰 로고 추가

--- app/src/main/java/com/konkuk/arabyte_aos/presentation/ui/login/LoginScreen.kt
+++ app/src/main/java/com/konkuk/arabyte_aos/presentation/ui/login/LoginScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
@@ -17,6 +18,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -82,14 +84,16 @@ fun LoginScreen(
             modifier = Modifier.fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Spacer(modifier = Modifier.weight(207f))
+            Spacer(modifier = Modifier.weight(140f))
+            Icon(imageVector = ImageVector.vectorResource(R.drawable.ic_all_logo), contentDescription = null, tint = Color.Unspecified)
+            Spacer(modifier = Modifier.height(10.dp))
             Text(
                 text = stringResource(R.string.splash_service_description),
                 style = ArabyteTheme.typography.bodyMed15,
                 textAlign = TextAlign.Center,
                 color = ArabyteTheme.colors.white,
             )
-            Spacer(modifier = Modifier.weight(459f))
+            Spacer(modifier = Modifier.weight(361f))
         }
         Row(
             modifier =
