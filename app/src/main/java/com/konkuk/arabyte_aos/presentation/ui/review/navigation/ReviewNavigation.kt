bb4e285769534cb092e781ac14bb6c3f427b58ee
hwidung <hwhwhw74@naver.com>
2025-04-15 00:11:53 +0900

    feat/#19: Bottom Navigation - Review 네비게이션 구조

---
+++ app/src/main/java/com/konkuk/arabyte_aos/presentation/ui/review/navigation/ReviewNavigation.kt
@@ -0,0 +1,54 @@
+package com.konkuk.arabyte_aos.presentation.ui.review.navigation
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.konkuk.arabyte_aos.presentation.type.ArabyteCategoryType
+import com.konkuk.arabyte_aos.presentation.ui.review.ReviewRoute
+
+fun NavController.navigationReview(
+    categoryType: ArabyteCategoryType?,
+) {
+    val category = categoryType?.name ?: ""
+    navigate(
+        route = "${ReviewRoute.ROUTE}/$category",
+    ) {
+        popUpTo(graph.startDestinationId) { inclusive = true }
+        launchSingleTop = true
+    }
+}
+
+fun NavGraphBuilder.reviewNavGraph(
+    paddingValues: PaddingValues,
+    // navigateToReviewDetailScreen : () -> Unit
+) {
+    composable(
+        route = "${ReviewRoute.ROUTE}/{categoryType}",
+        arguments =
+            listOf(
+                navArgument("categoryType") {
+                    type = NavType.StringType
+                    defaultValue = ""
+                    nullable = true
+                },
+            ),
+    ) { backStackEntry ->
+        val categoryString = backStackEntry.arguments?.getString("categoryType")
+        val categoryType =
+            runCatching {
+                ArabyteCategoryType.valueOf(categoryString ?: "")
+            }.getOrNull()
+
+        ReviewRoute(
+            paddingValues = paddingValues,
+            categoryType = categoryType,
+        )
+    }
+}
+
+object ReviewRoute {
+    const val ROUTE = "Review"
+}
