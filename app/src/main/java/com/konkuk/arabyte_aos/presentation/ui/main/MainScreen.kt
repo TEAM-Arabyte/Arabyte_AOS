d8a5a2f24230e4aa9d18e7c931d6fb7210815c95
hwidung <hwhwhw74@naver.com>
2025-04-16 18:08:24 +0900

    feat/#19: 상태바 처리 및 하단 패딩 적용

--- app/src/main/java/com/konkuk/arabyte_aos/presentation/ui/main/MainScreen.kt
+++ app/src/main/java/com/konkuk/arabyte_aos/presentation/ui/main/MainScreen.kt
@@ -1,9 +1,17 @@
 package com.konkuk.arabyte_aos.presentation.ui.main
 
+import android.app.Activity
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.konkuk.arabyte_aos.presentation.type.MainNavigationBarItemType
 import com.konkuk.arabyte_aos.presentation.ui.component.navigator.MainBottomBar
@@ -12,9 +20,22 @@ import com.konkuk.arabyte_aos.presentation.ui.main.Navigator.MainNavigator
 import com.konkuk.arabyte_aos.presentation.ui.main.Navigator.rememberMainNavigator
 
 @Composable
+fun TransparentStatusBarEffect() {
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = Color.Transparent.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = true
+        }
+    }
+}
+
+@Composable
 fun MainScreen(
     navigator: MainNavigator = rememberMainNavigator(),
 ) {
+    TransparentStatusBarEffect()
     MainScreenContent(
         navigator = navigator,
     )
@@ -32,14 +53,15 @@ private fun MainScreenContent(
 
     Scaffold(
         modifier = modifier,
-        content = { padding ->
+        content = { innerPadding ->
             MainNavHost(
                 navigator = navigator,
-                paddingValues = padding,
+                paddingValues = PaddingValues(bottom = innerPadding.calculateBottomPadding()),
             )
         },
         bottomBar = {
             MainBottomBar(
+                modifier = Modifier.navigationBarsPadding(),
                 isVisible = navigator.showBottomBar(),
                 navigationBarItems = MainNavigationBarItemType.entries.toList(),
                 currentNavigationBarItem = navigator.currentMainNavigationBarItem,
