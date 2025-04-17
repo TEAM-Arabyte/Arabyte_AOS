860dcf5638434d957a62a7b017266d01c7767e4e
t1nm1ksun <minsuk07672@naver.com>
2025-04-17 18:09:04 +0900

    chore/#27: 폴더 네이밍 변경

--- app/src/main/java/com/konkuk/arabyte_aos/presentation/ui/review/ReviewScreen.kt
+++
@@ -1,8 +0,0 @@
-package com.konkuk.arabyte_aos.presentation.ui.review
-
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-
-@Composable
-fun ReviewScreen(modifier: Modifier = Modifier) {
-}
