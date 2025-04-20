11136deff33ad2178cbd689cab1b4a550222f030
t1nm1ksun <minsuk07672@naver.com>
2025-04-21 01:37:53 +0900

    feat/#34: 게시글 상세 UI 구현

---
+++ app/src/main/java/com/konkuk/arabyte_aos/presentation/ui/noticeboarddetail/NoticeBoardDetailScreen.kt
@@ -0,0 +1,87 @@
+package com.konkuk.arabyte_aos.presentation.ui.noticeboarddetail
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.konkuk.arabyte_aos.R
+import com.konkuk.arabyte_aos.presentation.model.NoticeBoardDetail
+import com.konkuk.arabyte_aos.presentation.ui.component.ArabyteTopAppBar
+import com.konkuk.arabyte_aos.presentation.ui.noticeboarddetail.component.NoticeBoardDetailCommentBoard
+import com.konkuk.arabyte_aos.presentation.ui.noticeboarddetail.component.NoticeBoardDetailContent
+import com.konkuk.arabyte_aos.presentation.ui.reviewlist.ReviewListContract
+import com.konkuk.arabyte_aos.ui.theme.ArabyteAOSTheme
+import com.konkuk.arabyte_aos.ui.theme.ArabyteTheme
+
+@Composable
+fun NoticeBoardDetailRoute(
+    modifier: Modifier = Modifier,
+    viewModel: NoticeBoardDetailViewModel = hiltViewModel(),
+    innerPaddingValues: PaddingValues = PaddingValues(0.dp),
+) {
+    LaunchedEffect (Unit){ viewModel.setEvent(NoticeBoardDetailContract.NoticeBoardDetailEvent.LoadNoticeBoardDetail) }
+
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    NoticeBoardDetailScreen(uiState = uiState, innerPaddingValues = innerPaddingValues, modifier = modifier)
+}
+
+@Composable
+fun NoticeBoardDetailScreen(
+    modifier: Modifier = Modifier, innerPaddingValues: PaddingValues = PaddingValues(0.dp),
+    uiState: NoticeBoardDetailContract.NoticeBoardDetailUiState = NoticeBoardDetailContract.NoticeBoardDetailUiState(),
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(ArabyteTheme.colors.white)
+            .padding(innerPaddingValues)
+    ) {
+        ArabyteTopAppBar(
+            useBack = true,
+            onBackClick = {},
+            optionalIconRes = R.drawable.ic_all_optional_button_45,
+            onOptionalClick = {},
+        )
+        LazyColumn {
+            item {
+                NoticeBoardDetailContent(
+                    writerProfileImage = uiState.noticeBoardDetail.profileImage,
+                    writerNickname = uiState.noticeBoardDetail.nickname,
+                    writeDate = uiState.noticeBoardDetail.writeDate,
+                    title = uiState.noticeBoardDetail.title,
+                    content = uiState.noticeBoardDetail.content,
+                    isLiked = uiState.noticeBoardDetail.isLiked,
+                )
+            }
+            item {
+                HorizontalDivider(thickness = 8.dp, color = ArabyteTheme.colors.gray01)
+            }
+            item {
+                NoticeBoardDetailCommentBoard(
+                    commentList = uiState.noticeBoardDetail.commentList,
+                )
+            }
+        }
+
+
+    }
+}
+
+@Preview
+@Composable
+private fun NoticeBoardDetailScreenPreview() {
+    ArabyteAOSTheme {
+        NoticeBoardDetailScreen()
+    }
+}
