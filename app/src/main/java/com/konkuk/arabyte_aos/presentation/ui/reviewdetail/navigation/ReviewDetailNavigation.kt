c7651aea82212e6bc7f2c080c2973282357fa6e7
t1nm1ksun <minsuk07672@naver.com>
2025-05-04 22:08:05 +0900

    chore/#48: 리뷰 상세 네비게이션 import문 추가

--- app/src/main/java/com/konkuk/arabyte_aos/presentation/ui/reviewdetail/navigation/ReviewDetailNavigation.kt
+++ app/src/main/java/com/konkuk/arabyte_aos/presentation/ui/reviewdetail/navigation/ReviewDetailNavigation.kt
@@ -3,7 +3,9 @@ package com.konkuk.arabyte_aos.presentation.ui.reviewdetail.navigation
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import com.konkuk.arabyte_aos.presentation.ui.navigation.navigateBottomMain
 import com.konkuk.arabyte_aos.presentation.ui.reviewdetail.ReviewDetailRoute
 
@@ -18,8 +20,8 @@ fun NavGraphBuilder.reviewDetailNavGraph(
         route = ReviewDetailRoute.ROUTE_WITH_ARGUMENT,
         arguments =
             listOf(
-                androidx.navigation.navArgument(ReviewDetailRoute.ARGUMENT) {
-                    type = androidx.navigation.NavType.IntType
+                navArgument(ReviewDetailRoute.ARGUMENT) {
+                    type = NavType.IntType
                 },
             ),
     ) { backStackEntry ->
