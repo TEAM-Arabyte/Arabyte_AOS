3c863b7678bfd391ab139b0dc140728cac33900d
t1nm1ksun <minsuk07672@naver.com>
2025-05-05 15:24:44 +0900

    feat/#48: 유저 아이디 로컬에 저장

--- app/src/main/java/com/konkuk/arabyte_aos/data/datalocal/datasourceimpl/UserInfoLocalDataSourceImpl.kt
+++ app/src/main/java/com/konkuk/arabyte_aos/data/datalocal/datasourceimpl/UserInfoLocalDataSourceImpl.kt
@@ -7,6 +7,7 @@ import androidx.security.crypto.EncryptedSharedPreferences
 import androidx.security.crypto.MasterKey
 import com.kakao.sdk.auth.Constants.ACCESS_TOKEN
 import com.kakao.sdk.auth.Constants.REFRESH_TOKEN
+import com.kakao.sdk.user.model.User
 import com.konkuk.arabyte_aos.BuildConfig
 import com.konkuk.arabyte_aos.data.datalocal.datasource.UserInfoLocalDataSource
 import dagger.hilt.android.qualifiers.ApplicationContext
@@ -15,52 +16,59 @@ import javax.inject.Singleton
 
 @Singleton
 class UserInfoLocalDataSourceImpl
-    @Inject
-    constructor(
-        @ApplicationContext context: Context,
-    ) : UserInfoLocalDataSource {
-        private val masterKey =
-            MasterKey.Builder(context, MasterKey.DEFAULT_MASTER_KEY_ALIAS)
-                .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
-                .build()
+@Inject
+constructor(
+    @ApplicationContext context: Context,
+) : UserInfoLocalDataSource {
+    private val masterKey =
+        MasterKey.Builder(context, MasterKey.DEFAULT_MASTER_KEY_ALIAS)
+            .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+            .build()
 
-        private val sharedPreferences: SharedPreferences =
-            if (BuildConfig.DEBUG) {
-                context.getSharedPreferences(FILE_NAME, Context.MODE_PRIVATE)
-            } else {
-                EncryptedSharedPreferences.create(
-                    context,
-                    FILE_NAME,
-                    masterKey,
-                    EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
-                    EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM,
-                )
-            }
-
-        override var accessToken: String
-            get() = getValue(ACCESS_TOKEN)
-            set(value) = setValue(ACCESS_TOKEN, value)
-
-        override var refreshToken: String
-            get() = getValue(REFRESH_TOKEN)
-            set(value) = setValue(REFRESH_TOKEN, value)
-
-        override fun clear() {
-            setValue(REFRESH_TOKEN, "")
-            setValue(ACCESS_TOKEN, "")
+    private val sharedPreferences: SharedPreferences =
+        if (BuildConfig.DEBUG) {
+            context.getSharedPreferences(FILE_NAME, Context.MODE_PRIVATE)
+        } else {
+            EncryptedSharedPreferences.create(
+                context,
+                FILE_NAME,
+                masterKey,
+                EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM,
+            )
         }
 
-        private fun getValue(key: String): String =
-            sharedPreferences.getString(key, INITIAL_VALUE).orEmpty()
+    override var accessToken: String
+        get() = getValue(ACCESS_TOKEN)
+        set(value) = setValue(ACCESS_TOKEN, value)
 
-        private fun setValue(
-            key: String,
-            value: String,
-        ) =
-            sharedPreferences.edit { putString(key, value) }
+    override var refreshToken: String
+        get() = getValue(REFRESH_TOKEN)
+        set(value) = setValue(REFRESH_TOKEN, value)
 
-        companion object {
-            private const val FILE_NAME = "ArabyteLocalDataSource"
-            private const val INITIAL_VALUE = ""
-        }
+    override var userId: String
+        get() = getValue(USER_ID)
+        set(value) = setValue(USER_ID, value)
+
+    override fun clear() {
+        setValue(REFRESH_TOKEN, INITIAL_VALUE)
+        setValue(ACCESS_TOKEN, INITIAL_VALUE)
+        setValue(USER_ID, INITIAL_VALUE)
+
     }
+
+    private fun getValue(key: String): String =
+        sharedPreferences.getString(key, INITIAL_VALUE).orEmpty()
+
+    private fun setValue(
+        key: String,
+        value: String,
+    ) =
+        sharedPreferences.edit { putString(key, value) }
+
+    companion object {
+        private const val FILE_NAME = "ArabyteLocalDataSource"
+        private const val INITIAL_VALUE = ""
+        private const val USER_ID = "userId"
+    }
+}
