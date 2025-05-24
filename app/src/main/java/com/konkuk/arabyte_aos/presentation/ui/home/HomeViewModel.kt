7ab0e0da992289425b8e179b1cb231e021e5ec86
hwidung <hwhwhw74@naver.com>
2025-05-25 05:42:44 +0900

    Feat/#59: 홈 화면 리뷰/게시글 3개 UI 및 뷰모델 구현

--- app/src/main/java/com/konkuk/arabyte_aos/presentation/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/konkuk/arabyte_aos/presentation/ui/home/HomeViewModel.kt
@@ -1,13 +1,24 @@
 package com.konkuk.arabyte_aos.presentation.ui.home
 
+import androidx.lifecycle.viewModelScope
+import com.konkuk.arabyte_aos.domain.model.NoticeBoardList
+import com.konkuk.arabyte_aos.domain.model.ReviewList
+import com.konkuk.arabyte_aos.domain.usecase.home.GetHomeNoticeBoardsUseCase
+import com.konkuk.arabyte_aos.domain.usecase.home.GetHomeReviewsUseCase
 import com.konkuk.arabyte_aos.presentation.util.base.BaseViewModel
+import com.konkuk.arabyte_aos.presentation.util.log.DebugLog
+import com.konkuk.arabyte_aos.presentation.util.view.LoadState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel
     @Inject
-    constructor() : BaseViewModel<HomeContract.HomeUiState, HomeContract.HomeSideEffect, HomeContract.HomeEvent>() {
+    constructor(
+        private val getHomeReviewsUseCase: GetHomeReviewsUseCase,
+        private val getHomeNoticeBoardsUseCase: GetHomeNoticeBoardsUseCase,
+    ) : BaseViewModel<HomeContract.HomeUiState, HomeContract.HomeSideEffect, HomeContract.HomeEvent>() {
         override fun createInitialState(): HomeContract.HomeUiState = HomeContract.HomeUiState()
 
         override suspend fun handleEvent(event: HomeContract.HomeEvent) {
@@ -20,15 +31,33 @@ class HomeViewModel
         }
 
         private fun loadNoticeBoardList() {
-            // Todo : 서버로 부터 리스트 받아서, size 3으로
+            viewModelScope.launch {
+                val result: Result<NoticeBoardList> = getHomeNoticeBoardsUseCase()
+                result.onSuccess { noticeBoard ->
+                    setState { copy(noticeBoardItem = noticeBoard.content) }
+                }
+                    .onFailure { throwable ->
+                        DebugLog.e("HomeViewModel", "불러오기 실패: ${throwable.message}")
+                        setState { copy(loadState = LoadState.Error) }
+                    }
+            }
+        }
+
+        private fun loadReviewList() {
+            viewModelScope.launch {
+                val result: Result<ReviewList> = getHomeReviewsUseCase()
+                result.onSuccess { review ->
+                    setState { copy(reviewList = review.content) }
+                }
+                    .onFailure { throwable ->
+                        DebugLog.e("HomeViewModel", "불러오기 실패: ${throwable.message}")
+                        setState { copy(loadState = LoadState.Error) }
+                    }
+            }
         }
 
         private fun loadRegion() {
             // Todo : 서버로 부터 지역 받기
-        }
-
-        private fun loadReviewList() {
-            // Todo : 서버로 부터 리스트 받아서, size 3으로
         }
 
         private fun loadUserName() {
