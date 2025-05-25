5c9eeb7ec665bd0d66d037851a5bf8fdf0830ca2
hwidung <hwhwhw74@naver.com>
2025-05-26 01:17:26 +0900

    feat/#58: 게시물 개수 10개로 변경

--- app/src/main/java/com/konkuk/arabyte_aos/presentation/ui/noticeboard/NoticeBoardListViewModel.kt
+++ app/src/main/java/com/konkuk/arabyte_aos/presentation/ui/noticeboard/NoticeBoardListViewModel.kt
@@ -35,7 +35,7 @@ class NoticeBoardListViewModel
                     getNoticeBoardListUseCase(
                         articleKind = noticeBoardCategoryType.toApiValue(),
                         page = 0,
-                        size = 3,
+                        size = 10,
                         sort = "createdAt,desc",
                     )
                 result.onSuccess { articleList ->
